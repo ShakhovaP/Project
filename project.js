cb00e02558cd9914197fe93c22426546e54bdf17
Polina <43153453+ShakhovaP@users.noreply.github.com>
2019-06-11 12:25:08 +0300

    Add files via upload

--- project.js
+++ project.js
@@ -12,28 +12,28 @@ const graph = [
 
 //Unfolding graph: returns matrix with start and finish vertices and distances between 
 const unfoldGraph = () => {
-    const edges = []
+    const edges = [];
     graph.forEach((arr) => {
-        const strategy = elem => elem !== 0
+        const strategy = elem => elem !== 0;
         arr.filter(strategy).forEach((elem) => {
-            const edgAdd = [graph.indexOf(arr)+1, arr.indexOf(elem)+1, elem]
-            edges.push(edgAdd)
+            const edgAdd = [graph.indexOf(arr)+1, arr.indexOf(elem)+1, elem];
+            edges.push(edgAdd);
         })
         
     })
-    const ascending = (x, y) => (x[2]>y[2] ? 1 : -1)
-    edges.sort(ascending)
+    const ascending = (x, y) => (x[2]>y[2] ? 1 : -1);
+    edges.sort(ascending);
     return edges
 }
 const sortEdges = unfoldGraph()
 
 //Check : return true if elem is in mat
 const check = mat => elem => {
-    let flag = false
+    let flag = false;
     for (let cell of mat){
         for (let i of cell){
             if (i===elem){
-                flag = true
+                flag = true;
             }
         }
     }
@@ -42,7 +42,7 @@ const check = mat => elem => {
 
 
 const wrap = fn => (arr) => {
-    let flag = true
+    let flag = true;
     for (let vert=1; vert<=graph.length; vert++){
         if(fn(arr)(vert)===false){
             flag = false
@@ -54,15 +54,15 @@ const wrap = fn => (arr) => {
 const exit = wrap(check)
 
 const kraskal = () => {
-    const usedK = [[sortEdges[0][0], sortEdges[0][1]]]
+    const usedK = [[sortEdges[0][0], sortEdges[0][1]]];
 
-    const inUsed = check(usedK)
+    const inUsed = check(usedK);
     for (let i=1; i<sortEdges.length; i++) {        
         if (exit(usedK)===true){
             break
         }
-        const start = sortEdges[i][0]
-        const finish = sortEdges[i][1]
+        const start = sortEdges[i][0];
+        const finish = sortEdges[i][1];
 
         if (inUsed(start)===true && inUsed(finish)===true){
             continue
@@ -74,15 +74,15 @@ const kraskal = () => {
 }
 
 const prim = () => { 
-    const usedP = [[sortEdges[0][0], sortEdges[0][1]]]
+    const usedP = [[sortEdges[0][0], sortEdges[0][1]]];
 
-    const inUsed = check(usedP)
+    const inUsed = check(usedP);
     for (let i=1; i<sortEdges.length; i++) {
         if (exit(usedP)===true){
             break
         }
         const strategy = (elem) => inUsed(elem[0])===true && inUsed(elem[1])===false
-        const edge = sortEdges.find(strategy)
+        const edge = sortEdges.find(strategy);
         usedP.push([edge[0], edge[1]])
     }
     return usedP
@@ -97,22 +97,22 @@ const test = (fn, name) => {
     const time = []
     const memory = []
     for (let i=0; i<10; i++){
-        const startTime = process.hrtime()
+        const startTime = process.hrtime();
         fn()
-        const endTime = process.hrtime(startTime)
-        time.push(endTime[1])
+        const endTime = process.hrtime(startTime);
+        time.push(endTime[1]);
     }
     const timeMs = middle(time)/1000000
 
     for (let i=0; i<10; i++){
-        const startMem = process.memoryUsage().heapUsed
-        fn()
-        const endMem = process.memoryUsage().heapUsed
-        const memoryResult = endMem - startMem
-        memory.push(memoryResult)
+        const startMem = process.memoryUsage().heapUsed;
+        fn();
+        const endMem = process.memoryUsage().heapUsed;
+        const memoryResult = endMem - startMem;
+        memory.push(memoryResult);
     }
-    const memoryKb = middle(memory)/1000
-    const res = fn()
+    const memoryKb = middle(memory)/1000;
+    const res = fn();
     return {
         name: name,
         time: timeMs + ' ms',   //in ms
@@ -123,12 +123,12 @@ const test = (fn, name) => {
 
 
 //Kraskal test
-const testK = test(kraskal, 'kraskal')
-console.dir(testK)
+const testK = test(kraskal, 'kraskal');
+console.dir(testK);
 
 //Prim test
-const testP = test(prim, 'prim')
-console.dir(testP)
+const testP = test(prim, 'prim');
+console.dir(testP);
 
 
 //
